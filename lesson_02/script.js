7786e2cf21ccff7c0a95751355758279218cbef3
Alexender <dasdias@yandex.ru>
2023-11-17 17:50:45 +0300

    initial

---
+++ lesson_02/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const productName = "Планшет Samsung Galaxy Tab A8 10.5";
+const productQuantity = 3;
+const productCategory = "Планшеты";
+const productPrice = 25999;
+
+console.log('productName: ', productName);
+console.log('Общая сумма товара: ', productPrice * productQuantity);
