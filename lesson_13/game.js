046b3cc9fb2db107bf3b846f308aadb01e21c07f
Alex Fix <dasdias@yandex.ru>
2023-12-24 16:58:31 +0300

    lesson 13

--- lesson_13/game.js
+++ lesson_13/game.js
@@ -1,1 +1,24 @@
-'use strict';
+(() => {
+  const FIGURES_RUS = ['камень', 'ножницы', 'бумага'];
+
+  const getRandomMinMax = (min, max) => {
+    if (min > max) {
+      [min, max] = [max, min];
+    }
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+  console.log('getRandomMinMax: ', getRandomMinMax(1, 3));
+
+  const game = () => {
+    const result = {
+      player: 0,
+      computer: 0,
+    };
+    return function start() {
+
+    };
+  };
+  window.startGame = game;
+})();
